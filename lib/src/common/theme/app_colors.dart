c0b80e22be1eb7a8a67f5e74a66c2092b9819a36
Aleksey Kuzmin <kboomska@gmail.com>
2023-07-31 18:42:19 +0300

    App colors

---
+++ lib/src/common/theme/app_colors.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const appBackground = Color.fromRGBO(255, 255, 255, 1);
+  static const appBarIcon = Color.fromRGBO(0, 0, 0, 1);
+
+  static const textHeadline = Color.fromRGBO(0, 0, 0, 1);
+  static const textSubhead = Color.fromRGBO(0, 0, 0, 0.5);
+  static const textProductDescription = Color.fromRGBO(0, 0, 0, 0.65);
+  static const textProductWeight = Color.fromRGBO(0, 0, 0, 0.4);
+
+  static const bottomNavigationBarBorder = Color.fromRGBO(232, 233, 236, 1);
+  static const bottomNavigationBarSelected = Color.fromRGBO(51, 100, 224, 1);
+  static const bottomNavigationBarUnselected = Color.fromRGBO(165, 169, 178, 1);
+  static const bottomNavigationBarBackground = Color.fromRGBO(255, 255, 255, 1);
+
+  static const dishItemBackground = Color.fromRGBO(248, 247, 245, 1);
+
+  static const dishTagBackgroundSelected = Color.fromRGBO(51, 100, 224, 1);
+  static const dishTagBackgroundUnselected = Color.fromRGBO(248, 247, 245, 1);
+  static const dishTagTextSelected = Color.fromRGBO(255, 255, 255, 1);
+  static const dishTagTextUnselected = Color.fromRGBO(0, 0, 0, 1);
+
+  static const buttonBackground = Color.fromRGBO(51, 100, 224, 1);
+  static const buttonPressed = Color.fromRGBO(255, 255, 255, 0.34);
+  static const buttonText = Color.fromRGBO(255, 255, 255, 1);
+
+  static const shoppingCartCounterBackground = Color.fromRGBO(239, 238, 236, 1);
+}
